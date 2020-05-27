4ce670725bd706d22b23dc2b94db0c544a59df69
OtaXross <otaxross@gmail.com>
2020-05-27 19:42:03 +0900

    save1

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <title>Amazon(仮)</title>
 </head>
 <body>
-    <header></header>
+    <header>aaaa</header>
+    <main>
+    <!-- 大筋のリンク -->
+        <a class="new" href="商品IDetc">商品登録</a>
+        <a class="confirm" href="公開設定etc">商品確認</a>
+        <a class="accounts" href="企業名etc">アカウント</a>
+    </main>
 </body>
 </html>
---
+++ regist.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">;
+    <title>商品登録</title>
+</head>
+<body>
+    <header>
+     <h1>商品を登録する</h1>
+    </header>
+    <main>
+     <input type="text" name="name" id="name">
+     <label for="name">商品名</label>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
