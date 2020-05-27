e61d0dd5dae825ca53c63abe29b5f2d5d2b84175
OtaXross <otaxross@gmail.com>
2020-05-27 19:53:38 +0900

    save2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <header>aaaa</header>
     <main>
     <!-- 大筋のリンク -->
-        <a class="new" href="商品IDetc">商品登録</a>
+        <a class="new" href="regist.php">商品登録</a>
         <a class="confirm" href="公開設定etc">商品確認</a>
         <a class="accounts" href="企業名etc">アカウント</a>
     </main>
--- regist.php
+++ regist.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">;
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>商品登録</title>
 </head>
 <body>
@@ -10,8 +10,17 @@
      <h1>商品を登録する</h1>
     </header>
     <main>
-     <input type="text" name="name" id="name">
+    <form>
+     <label for="id">商品ID</label>
+     <input type="text" name="id" id="id">
+     <label for="image">商品の画像</label>
+     <input type="file" name="image" id="image">
      <label for="name">商品名</label>
+     <input type="text" name="name" id="name">
+     <label for="desc">商品説明</label>
+     <textarea name="desc" id="desc" cols="30" rows="10"></textarea>
+     <label for=""></label>
+     </form>
     </main>
     <footer>
     </footer>
